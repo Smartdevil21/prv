6b9a29250cfe85e7664cc47b390e36952b6cbd06
Pratik1803 <pratikvaidya1803@gmail.com>
2022-04-13 16:57:59 +0530

    Contact form integrated with bork/feedback

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,9 +2,16 @@ import { Button, TextField } from "@mui/material";
 import React, { useEffect, useContext, useState } from "react";
 import Styles from "./Contact.module.scss";
 import { StatesContext } from "../../App";
+import axios from "axios";
 
 function Contact() {
+	const [loading, setLoading] = useState(false);
 	const { states, setStates } = useContext(StatesContext);
+	const [messageDetails, setMessageDetails] = useState({
+		name: "",
+		email: "",
+		message: "",
+	});
 
 	useEffect(() => {
 		setStates((prev) => ({ ...prev, reversed: false }));
@@ -16,6 +23,28 @@ function Contact() {
 		},
 	});
 
+	const submit = async () => {
+		try {
+			setLoading(true);
+			const result = await axios({
+				method: "post",
+				url: "https://bork0.herokuapp.com/feedback",
+				data: {
+					username: messageDetails.name,
+					email: messageDetails.email,
+					message: messageDetails.message,
+				},
+			});
+			alert("Message Sent! ✈️");
+			setMessageDetails({
+				name:"",
+				email:"",
+				message:""
+			});
+			setLoading(false);
+		} catch (error) {}
+	};
+
 	return (
 		<section className={Styles.contact}>
 			<h1>Get In Touch!</h1>
@@ -26,12 +55,20 @@ function Contact() {
 						size="small"
 						placeholder="Name"
 						color="success"
+						value={messageDetails.name}
+						onChange={(e) => {
+							setMessageDetails((prev) => ({ ...prev, name: e.target.value }));
+						}}
 					/>
 					<TextField
 						variant="filled"
 						size="small"
 						placeholder="Email"
 						color="success"
+						value={messageDetails.email}
+						onChange={(e) => {
+							setMessageDetails((prev) => ({ ...prev, email: e.target.value }));
+						}}
 					/>
 					<TextField
 						variant="filled"
@@ -53,7 +90,15 @@ function Contact() {
 					fullWidth
 					className={Styles.message_input}
 				/> */}
-				<textarea className={Styles.message_input} rows={10} placeholder="Message" />
+				<textarea
+					className={Styles.message_input}
+					value={messageDetails.message}
+					onChange={(e) => {
+						setMessageDetails((prev) => ({ ...prev, message: e.target.value }));
+					}}
+					rows={10}
+					placeholder="Message"
+				/>
 				<div className={Styles.bottom_div}>
 					<div
 						onMouseEnter={(e) => {
@@ -75,9 +120,10 @@ function Contact() {
 							}px`;
 						}}
 						className={`${Styles.contact_btn}`}
+						onClick={submit}
 					>
 						<div className={`${Styles.test} test`}></div>
-						<p>Send</p>
+						<p>{loading ? "Loading" : "Send"}</p>
 					</div>
 				</div>
 			</div>
